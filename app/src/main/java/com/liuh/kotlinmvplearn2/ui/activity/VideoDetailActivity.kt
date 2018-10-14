7f3b4c7611d79f03db7d85a21c573541ee8087f6
liuhuan2015 <2210712252@qq.com>
2018-10-14 20:05:53 +0800

    视频详情

--- app/src/main/java/com/liuh/kotlinmvplearn2/ui/activity/VideoDetailActivity.kt
+++ app/src/main/java/com/liuh/kotlinmvplearn2/ui/activity/VideoDetailActivity.kt
@@ -1,13 +1,28 @@
 package com.liuh.kotlinmvplearn2.ui.activity
 
 import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.os.Build
+import android.support.v4.view.ViewCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.transition.Transition
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import com.liuh.kotlinmvplearn2.R
 import com.liuh.kotlinmvplearn2.base.BaseActivity
+import com.liuh.kotlinmvplearn2.glide.GlideApp
 import com.liuh.kotlinmvplearn2.mvp.contract.VideoDetailContract
 import com.liuh.kotlinmvplearn2.mvp.model.bean.HomeBean
 import com.liuh.kotlinmvplearn2.mvp.presenter.VideoDetailPresenter
+import com.liuh.kotlinmvplearn2.showToast
 import com.liuh.kotlinmvplearn2.ui.adapter.VideoDetailAdapter
+import com.liuh.kotlinmvplearn2.view.VideoListener
+import com.scwang.smartrefresh.header.MaterialHeader
+import com.shuyu.gsyvideoplayer.listener.LockClickListener
 import java.text.SimpleDateFormat
+import com.shuyu.gsyvideoplayer.utils.OrientationUtils
+import kotlinx.android.synthetic.main.activity_video_detail.*
 
 @SuppressLint("SimpleDateFormat")
 /**
@@ -35,19 +50,171 @@ class VideoDetailActivity : BaseActivity(), VideoDetailContract.View {
 
     private lateinit var itemData: HomeBean.Issue.Item
 
+    private var orientationUtils: OrientationUtils? = null
+
+    private var isPlay: Boolean = false
+    private var isPause: Boolean = false
+
+    private var isTransition: Boolean = false
+
+    private var transition: Transition? = null
+
+    private var mMaterialHeader: MaterialHeader? = null
 
     override fun layoutId(): Int = R.layout.activity_video_detail
 
+    /**
+     * 初始化数据
+     */
     override fun initData() {
 
     }
 
     override fun initView() {
         mPresenter.attachView(this)
+        // 过渡动画
+        initTransition()
+
+        initVideoViewConfig()
+
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        mRecyclerView.adapter = mAdapter
+
+        // 设置相关视频 Item 的点击事件
+        mAdapter.setOnItemDetailClick { mPresenter.loadVideoInfo(it) }
+
+
+    }
+
+    private fun initTransition() {
+        if (isTransition && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            postponeEnterTransition()
+            ViewCompat.setTransitionName(mVideoView, IMG_TRANSITION)
+            addTransitionListener()
+            startPostponedEnterTransition()
+        } else {
+            loadVideoInfo()
+        }
+    }
+
+    /**
+     * 初始化 VideoView 的配置
+     */
+    private fun initVideoViewConfig() {
+
+        // 设置旋转
+        orientationUtils = OrientationUtils(this, mVideoView)
+
+        // 是否旋转
+        mVideoView.isRotateViewAuto = false
+
+        // 是否可以滑动调整
+        mVideoView.setIsTouchWiget(true)
+
+        // 增加封面
+        val imageView = ImageView(this)
+
+        imageView.scaleType = ImageView.ScaleType.CENTER_CROP
+
+        GlideApp.with(this)
+                .load(itemData.data?.cover?.feed)
+                .centerCrop()
+                .into(imageView)
+
+        mVideoView.thumbImageView = imageView
+
+        mVideoView.setStandardVideoAllCallBack(object : VideoListener {
+
+            override fun onPrepared(url: String, vararg objects: Any) {
+                super.onPrepared(url, *objects)
+
+                // 开始播放了才能旋转和全屏
+                orientationUtils?.isEnable = true
+                isPlay = true
+            }
+
+            override fun onAutoComplete(url: String, vararg objects: Any) {
+                super.onAutoComplete(url, *objects)
+                Log.e("-----", "onAutoComplete---自动播放完成")
+            }
+
+            override fun onPlayError(url: String, vararg objects: Any) {
+                super.onPlayError(url, *objects)
+                showToast("播放失败")
+            }
+
+            override fun onEnterFullscreen(url: String, vararg objects: Any) {
+                super.onEnterFullscreen(url, *objects)
+                Log.e("-----", "---进入全屏")
+            }
+
+            override fun onQuitFullscreen(url: String, vararg objects: Any) {
+                super.onQuitFullscreen(url, *objects)
+                Log.e("-----", "退出全屏")
+                // 列表返回的样式判断
+                orientationUtils?.backToProtVideo()
+            }
+        })
+
+        // 设置返回按键功能
+        mVideoView.backButton.setOnClickListener({ onBackPressed() })
+
+        // 设置全屏按键功能
+        mVideoView.fullscreenButton.setOnClickListener {
+            // 直接横屏
+            orientationUtils?.resolveByClick()
+            //  是否有actionBar，有的话需要隐藏； 是否有statusBar，有的话需要隐藏
+            mVideoView.startWindowFullscreen(this, true, true)
+        }
+
+        // 锁屏事件
+        mVideoView.setLockClickListener(object : LockClickListener {
+            override fun onClick(view: View?, lock: Boolean) {
+                orientationUtils?.isEnable = !lock
+            }
+        })
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun addTransitionListener() {
+        transition = window.sharedElementEnterTransition
+        transition?.addListener(object : Transition.TransitionListener {
+            override fun onTransitionEnd(transition: Transition?) {
+                Log.e("-----", "onTransitionEnd")
+
+                loadVideoInfo()
+                transition?.removeListener(this)
+            }
+
+            override fun onTransitionResume(transition: Transition?) {
+
+            }
+
+            override fun onTransitionPause(transition: Transition?) {
+
+            }
+
+            override fun onTransitionCancel(transition: Transition?) {
+
+            }
+
+            override fun onTransitionStart(transition: Transition?) {
+
+            }
+        })
+    }
+
+    /**
+     * 加载视频信息
+     */
+    private fun loadVideoInfo() {
+        mPresenter.loadVideoInfo(itemData)
     }
 
     override fun start() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 
--- app/src/main/java/com/liuh/kotlinmvplearn2/ui/adapter/VideoDetailAdapter.kt
+++ app/src/main/java/com/liuh/kotlinmvplearn2/ui/adapter/VideoDetailAdapter.kt
@@ -2,8 +2,12 @@ package com.liuh.kotlinmvplearn2.ui.adapter
 
 import android.content.Context
 import android.graphics.Typeface
+import android.widget.ImageView
+import android.widget.TextView
 import com.liuh.kotlinmvplearn2.MyApplication
 import com.liuh.kotlinmvplearn2.R
+import com.liuh.kotlinmvplearn2.durationFormat
+import com.liuh.kotlinmvplearn2.glide.GlideApp
 import com.liuh.kotlinmvplearn2.mvp.model.bean.HomeBean
 import com.liuh.kotlinmvplearn2.view.recyclerview.MultipleType
 import com.liuh.kotlinmvplearn2.view.recyclerview.MyViewHolder
@@ -36,12 +40,68 @@ class VideoDetailAdapter(mContext: Context, data: ArrayList<HomeBean.Issue.Item>
     private var textTypeface: Typeface? = null
 
     init {
-        textTypeface=Typeface.createFromAsset(MyApplication.context.assets, "fonts/FZLanTingHeiS-L-GB-Regular.TTF")
+        textTypeface = Typeface.createFromAsset(MyApplication.context.assets, "fonts/FZLanTingHeiS-L-GB-Regular.TTF")
     }
 
+    /**
+     * 添加相关推荐等数据 Item
+     */
+    fun addData(item: ArrayList<HomeBean.Issue.Item>) {
+        mData.addAll(item)
+        notifyItemRangeInserted(1, item.size)
+    }
+
+    /**
+     * Kotlin中函数可以作为参数，写callback的时候，可以不用interface
+     */
+    private var mOnItemClickRelatedVideo: ((item: HomeBean.Issue.Item) -> Unit)? = null
+
+    fun setOnItemDetailClick(mItemRelatedVideo: (item: HomeBean.Issue.Item) -> Unit) {
+        this.mOnItemClickRelatedVideo = mItemRelatedVideo
+    }
+
+    /**
+     * 绑定数据
+     */
     override fun bindData(holder: MyViewHolder, data: HomeBean.Issue.Item, position: Int) {
+        when {
+            position == 0 -> setVideoDetailInfo(data, holder)
 
+            data.type == "textCard" -> {
+                holder.setText(R.id.tv_text_card, data.data?.text!!)
+                // 设置方正兰亭细黑简体
+                holder.getView<TextView>(R.id.tv_text_card).typeface = textTypeface
+            }
+
+            data.type == "videoSmallCard" -> {
+                with(holder) {
+                    setText(R.id.tv_title, data.data?.title!!)
+                    setText(R.id.tv_tag, "#${data.data?.category}/${durationFormat(data.data?.duration)}")
+                    setImagePath(R.id.iv_video_small_card, object : MyViewHolder.HolderImageLoader(data.data?.cover?.detail!!) {
+                        override fun loadImage(iv: ImageView, path: String) {
+                            GlideApp.with(mContext)
+                                    .load(path)
+                                    .optionalTransform(GlideRoundTransform())
+                                    .placeholder(R.drawable.placeholder_banner)
+                                    .into(iv)
+                        }
+                    })
+                }
+
+                // 判断 onItemClickRelatedVideo 并使用
+                holder.itemView.setOnClickListener { mOnItemClickRelatedVideo?.invoke(data) }
+            }
+
+            else -> throw IllegalAccessException("Api 解析出错了，出现其它类型")
+        }
+    }
+
+    /**
+     * 设置视频详情数据
+     */
+    private fun setVideoDetailInfo(data: HomeBean.Issue.Item, holder: MyViewHolder) {
 
 
     }
+
 }
